f5bc333750843fadc1b53ecef9c06999b4b6ad2b
Vinay jain <jainv823@gmail.com>
2023-07-16 18:14:58 +0530

    Added the Image and Article Text

--- app/src/main/java/com/example/composearticleapp/MainActivity.kt
+++ app/src/main/java/com/example/composearticleapp/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.composearticleapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composearticleapp.ui.theme.ComposeArticleAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +30,49 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting(getString(R.string.Title_txt), getString(R.string.para1_txt), getString(R.string.para2_txt))
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(heading: String, para1: String, para2: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.bg_compose_background)
+    Column {
+        Image(
+            painter = image,
+            contentDescription = null,
+        )
+
+        Text(
+            text = heading,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+        Text(
+            text = para1,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = para2,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeArticleAppTheme {
-        Greeting("Android")
+        Greeting(stringResource(R.string.Title_txt), stringResource(R.string.para1_txt), stringResource(R.string.para1_txt))
     }
 }
